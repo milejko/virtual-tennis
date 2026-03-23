627837a5a6eacb779680ddbfa238230faa7fcc87
Mariusz Miłejko <mariusz.milejko@sanoma.com>
2026-03-23 14:43:16 +0100

    PWA experiments

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'virtual-tennis-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/peerjs@1.5.4/dist/peerjs.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
